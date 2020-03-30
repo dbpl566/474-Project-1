388e964011cacff973d9654bfc0dce00816426d0
dbpl566 <54877378+dbpl566@users.noreply.github.com>
2020-03-29 23:48:27 -0500

    updates from test2

--- scripts/gameUI.js
+++ scripts/gameUI.js
@@ -1,3 +1,9 @@
+var gameHeight = $(window).height();
+var gameWidth = $(window).width();
+var boardHeight = 1884;
+var boardWidth = 680;
+/*const Direction = Object.freeze({LEFT, RIGHT});*/
+
 var gameUI = function () {
     var self = this;
     this.game = undefined;
@@ -6,7 +12,8 @@ var gameUI = function () {
     this.platforms = [];
     this.platformId = 0;
     this.player = undefined;
-    this.height = 1884;
+    this.height = boardHeight;
+    this.globalMoveSpeed = -1;
     this.top = {
         pos: 0,
         name: '',
@@ -15,22 +22,27 @@ var gameUI = function () {
 
     // not using game.js anymore right now
     this.initialize = function () {
-        self.player = new player();
         self.top.name = '#playBoard1';
-        $('#GameStopped').show();
-        $('#GameRunning').hide();
+        $('#startScreen').show();
+        $('#endScreen').hide();
 
-        $('#StartBtn').on('click', function () {
-            $('#GameStopped').hide();
-            $('#GameRunning').show();
-            self.running = true;
-            self.startGame();
+        $('#startBtn').on('click', function () {
+            $('#startBtn').hide();
+            $('#startScreen').slideUp();
+            setTimeout(self.startGame, 0); //number delays when game starts
+
         });
-        $('#StopBtn').on('click', function () {
-            $('#GameStopped').show();
-            $('#GameRunning').hide();
+
+        //call to show endscreen and restart
+        this.endGame = function () {
+            $('#restartBtn').show();
+            $('#endScreen').show();
             self.running = false;
+        }
+
+        $('#restartBtn').on('click', function () {
             //self.game.reset();
+            self.running = true;
             clearInterval(self.timer);
             self.counter = 0;
         });
@@ -55,7 +67,7 @@ var gameUI = function () {
             top2 = this.setBackgroundPos(top2.top);
 
             // determines which background to add platforms onto
-            if (top1 > -1100 && top1 < 700) {
+            if (top1 > -1100 && top1 < 800) {
                 self.top.name = '#playBoard1';
                 self.top.pos = top1;
             }
@@ -71,10 +83,12 @@ var gameUI = function () {
             //set the player div = to his current x and y
             $('#player').css("left", self.player.xPos + 'px');
             $('#player').css("top", self.player.yPos + 'px');
-        };
+
+        } /*end of refreshView*/
+
+
         // called every tick (50ms) from setInterval
         this.updateUI = function () {
-
             // counter gets incremented every tick from the setInterval (50ms right now)
             // after 100 ticks, add a new platform and remove any that are out of view
             if (self.counter % 100 == 0) {
@@ -83,32 +97,38 @@ var gameUI = function () {
 
             // update the yPos (internally) of each platform
             // if player is on a platform, its ySpeed gets set to -1 instead of default 2;
-            var playerYSpeed = 2;
+            self.player.ySpeed = 3;
             self.platforms.forEach(p => {
-                if ((self.player.yPos + 25) - p.yPos <= 1 && (self.player.yPos + 25) - p.yPos >= -1) {
-                    let minX = p.xPos - 24;
+                if ((self.player.yPos + self.player.height) - p.yPos <= 3 && (self.player.yPos + self.player.height) - p.yPos >= -3) {
+                    let minX = p.xPos - self.player.width;
                     let maxX = p.xPos + p.width;
                     if (self.player.xPos >= minX && self.player.xPos <= maxX) {
-                        playerYSpeed = -1;
+                        self.player.ySpeed = self.globalMoveSpeed;
+                        self.player.yAccel = 0;
+                        self.player.yPos = p.yPos - self.player.height;
                     }
                 }
-                p.yPos -= 1;
+                p.updatePosition();
             })
 
+            self.player.handleAccelTimer();
+
             // update the y and x internally of the player
-            self.player.yPos += playerYSpeed;
+            self.player.yPos += self.player.ySpeed + self.player.yAccel;
             self.player.xPos += self.player.xSpeed;
+
 
 
             self.refreshView();
 
             self.counter += 1;
 
-        }
+        }   /*end of updateUI*/
 
         this.startGame = function () {
             // creates a timer that calls updateUI every 50 ms
-            self.timer = setInterval(self.updateUI, 50)
+            self.timer = setInterval(self.updateUI, 30);
+            self.player = new player();
         }
 
         this.updatePlatforms = function () {
@@ -140,6 +160,7 @@ var gameUI = function () {
 
             // set the global platforms array = to the newArray that has removed the out of view platforms and added the new ones
             self.platforms = newArray;
+
         }
 
 
@@ -148,12 +169,13 @@ var gameUI = function () {
             if (curY <= -self.height) {
                 curY = self.height;
             }
-            curY -= 1;
+            curY += self.globalMoveSpeed;
             return curY;
         }
-    }
+    } /*end of initialize*/
     this.initialize();
-}
+
+} /*end of gameUI*/
 
 var platform = function (id, top) {
     var self = this;
@@ -167,27 +189,28 @@ var platform = function (id, top) {
     this.initialize = function (id, top) {
         self.id = id;
         self.speed = 10;
-        // give the platforms a random x pos
-        self.xPos = (Math.floor(Math.random() * 530));
         // determine where to pos the 'top' css element of the platform
-        self.top = 700 - top;
+        self.top = 800 - top; 
         // set the starting yPos
-        self.yPos = 700;
+        self.yPos = 800;
         self.width = 150;
         self.height = 25;
+        // give the platforms a random x pos
+        self.xPos = (Math.floor(Math.random() * (boardWidth - self.width)));
     };
     this.positionPlatform = function (x, y) {
         self.xPos = x;
         self.yPos = y;
+        self.ySpeed = 2;
     };
     this.setSpeed = function (speed) {
         self.speed = speed;
     };
     this.updatePosition = function () {
-        self.yPos -= 1; 
+        self.yPos -= 1;
     }
     this.initialize(id, top);
-}
+} /*end of platform*/
 
 var player = function () {
     var self = this;
@@ -195,16 +218,58 @@ var player = function () {
     this.xPos = 0;
     this.yPos = 0;
     this.xSpeed = 0;
+    this.ySpeed = 3;
+    this.yAccel = 0;
+    this.height = 0;
+    this.width = 0;
+    this.accelTimer = undefined;
+    this.state = 'idle';
+    this.prevstate;
 
     this.initialize = function () {
-        self.xPos = 20;
-        self.yPos = 20;
+        self.xPos = 328;
+        self.yPos = 32;
+        self.height = 32;
+        self.width = 32;
+        self.createAccelTimer();
     };
 
     this.setPos = function (x, y) {
         self.xPos = x;
         self.yPos = y;
     };
+
+    this.createAccelTimer = function () {
+        self.accelTimer = setInterval(self.updateAccel, 500);
+    }
+
+    this.handleAccelTimer = function () {
+        if (self.ySpeed >= 2) {
+            if (self.accelTimer == undefined) {
+                self.createAccelTimer();
+            }
+        }
+        if (self.ySpeed == -1) {
+            if (self.accelTimer != undefined) {
+                clearInterval(self.accelTimer);
+                self.accelTimer = undefined;
+            }
+        }
+    }
+
+    this.updateAccel = function () {
+        if (self.yAccel <= 4) {
+            self.yAccel += 1;
+        }
+    }
+
+    //changes state to input string and handles hiding and showing player images
+    this.changeState = function(newstate){
+        self.prevstate = self.state;
+        self.state = newstate;
+        $('#'+self.state).css("display", "block");
+        $('#'+self.prevstate).css("display", "none");
+    }
 
     // if the player is currently not moving (isMoving): sets the xSpeed of the player as well as isMoving to true
     this.handleKeyDown = function (e) {
@@ -213,10 +278,12 @@ var player = function () {
         }
         // left
         if (e.which == 37) {
+            self.changeState('runleft');
             self.xSpeed = -5;
         }
         // right
         else if (e.which == 39) {
+            self.changeState('runright');
             self.xSpeed = 5;
         }
 
@@ -225,14 +292,18 @@ var player = function () {
 
     // when left/right arrow is released, sets the xSpeed back to 0 and isMoving to false;
     this.handleKeyUp = function (e) {
-        if (e.which == 37 || e.which == 39) {
+        if (e.which == 37){
             self.xSpeed = 0;
             self.isMoving = false;
-
+            self.changeState('idleleft');
+        }else if (e.which == 39) {
+            self.xSpeed = 0;
+            self.isMoving = false;
+            self.changeState('idleright');
         }
     }
 
     this.initialize();
 
 
-}
+} /*end of player*/
